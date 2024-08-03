bb2b369c544c4e59fe85b4b952d31067a4128049
RasmiJati <jatirasmi65@gmail.com>
2024-08-03 07:57:50 +0545

    college database

---
+++ college.sql
@@ -0,0 +1,30 @@
+-- **************************************QUESTIONS************************************
+
+-- Create a database named "college" and use this database to create table named "Student". The Student table will consists of following fields:
+-- rollNumber int Primary key,
+-- fullName varchar (50),
+-- address varchar(50),
+-- marks decimal(10,2),
+-- 2. Insert atleast 10 data to fill this Student records.
+-- 3. Similarly, create table named "Teacher". The Teacher table will consists of following fields:
+-- id int primary key,
+-- name varchar(50),
+-- assignedStudentId int (make foreign key relation) => (using cascading logic - Optional),
+-- salary decimal(10,2)
+-- 4. Insert at least 10 Teacher records.
+-- 5. Display total marks from all the students (can use aggregate functions)
+-- 5. Calculate average marks and display all the student's fullName whose marks > 50
+-- 6. calculate and display top 3 students based on highest marks
+-- 7. calculate and display top 3 students based on lowest marks
+-- 8. Display only teacher name who name starts with letter "s"
+-- 9. Display only teacher name who name ends with letter "a"
+-- 10. Rename column "name" to "teacherName" on Teacher table
+-- 11. Display teacherName in descending order alphabetically
+-- 12. Display top 3 teacherName with highest salary
+
+-- *********************************************************************************************
+
+
+
+
+
